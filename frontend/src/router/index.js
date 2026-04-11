47b97f90d612b9040d97ca662ea1411ede9ecd20
zhwkml <zhawakamela123@gmail.com>
2026-04-12 00:22:04 +0700

    Selesai fitur tambah peminjaman dan dropdown

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,30 +1,37 @@
 import { createRouter, createWebHistory } from 'vue-router'
+// Import Komponen
 import DaftarPinjaman from '../views/peminjaman/DaftarPinjaman.vue'
 import TambahPinjaman from '../views/peminjaman/TambahPinjaman.vue'
+import AuthLayout from '../layouts/AuthLayout.vue' // Pastiin path ini bener ya, Wa!
 
-<<<<<<< Updated upstream
-=======
 const Login = () => import('@/views/auth/LoginPage.vue')
 
 const routes = [
-  { path: '/', redirect: '/dashboard' },
+  { 
+    path: '/', 
+    redirect: '/dashboard' 
+  },
 
-  // --- Ini Route punya Alan (Auth) ---
+  // --- Group Route Login (Punya Alan) ---
   {
     path: '/',
     component: AuthLayout,
     children: [
-      { path: 'login', name: 'Login', component: Login, meta: { guest: true } },
+      { 
+        path: 'login', 
+        name: 'Login', 
+        component: Login, 
+        meta: { guest: true } 
+      },
     ],
   },
 
-  // --- Ini Route punya Lu (Peminjaman) ---
+  // --- Group Route Peminjaman (Punya Lu) ---
   {
     path: '/peminjaman',
     name: 'peminjaman.index',
     component: DaftarPinjaman
   },
-
   {
     path: '/peminjaman/tambah',
     name: 'peminjaman.create',
@@ -32,22 +39,10 @@ const routes = [
   }
 ]
 
->>>>>>> Stashed changes
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/peminjaman',
-      name: 'peminjaman.index',
-      component: DaftarPinjaman
-    }
-    // BAGIAN LOGIN KITA MATIIN DULU SEMENTARA BIAR GAK ERROR
-    // ,{
-    //   path: '/login',
-    //   name: 'login',
-    //   component: () => import('../views/auth/LoginPage.vue')
-    // }
-  ]
+  // PANGGIL VARIABEL routes YANG DI ATAS TADI
+  routes: routes 
 })
 
 export default router
